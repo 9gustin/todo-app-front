91012c3155de9e35979d793d127e553a264a4e42
Agustin Vazquez <agustinvazquez453gmail.com>
2020-11-19 11:31:33 -0300

    refactor todo list components.

--- src/components/lists/list.js
+++ src/components/lists/list.js
@@ -2,50 +2,25 @@ import { ListItem } from "./listItems.js";
 import { IconDelete } from "../texts/icons.js";
 import { DeleteAllButton } from "../forms/formItems.js";
 
-const AllList = (props) => {
+const TodoList = props => {
     const todos = props.todos;
-    const handleToggleCompleted = props.handleToggleCompleted;
-
-    return (
-        <ul>
-            {todos.map((todo) => (
-                <ListItem key={`ListItem-${todo.id}`} item={todo} handleClick={handleToggleCompleted}/>
-            ))}
-        </ul>
-    );
-};
-
-const ActiveList = (props) => {
-    const todos = props.todos;
-    const handleToggleCompleted = props.handleToggleCompleted;
-
-    return (
-        <ul>
-            {todos.filter(todo => !todo.completed).map((todo) => (
-                <ListItem key={`ListItem-${todo.id}`} item={todo} handleClick={handleToggleCompleted}/>
-            ))}
-        </ul>
-    );
-};
-
-const CompletedList = (props) => {
-    const todos = props.todos;
+    const allowDelete = props.allowDelete;
     const handleDelete = props.handleDelete;
     const handleToggleCompleted = props.handleToggleCompleted;
     const handleDeleteCompleted = props.handleDeleteCompleted;
 
-    const getActions = todo => (<button key={`DeleteButton-${todo.id}`} className="flex items-center" onClick={() => { handleDelete(todo) }}> <IconDelete /> </button>);
-    const getTodos = () => todos.filter(todo => todo.completed);
+    const getActions = todo => (allowDelete ? <button key={`DeleteButton-${todo.id}`} className="flex items-center" onClick={() => { handleDelete(todo) }}> <IconDelete /> </button> : '');
+
     return (
         <>
             <ul>
-                {getTodos().map((todo) => (
+                {todos.map((todo) => (
                     <ListItem key={`ListItem-${todo.id}`} item={todo} actions={getActions(todo)} handleClick={handleToggleCompleted}/>
                 ))}
             </ul>
-            {getTodos() && getTodos().length > 0 ? <DeleteAllButton className="mt-5 float-right flex items-center bg-red-600 hover:bg-red-700 text-white text-xs py-2 px-4 rounded" handleClick={handleDeleteCompleted}/> : ''}
+            {allowDelete && todos?.length > 0 && <DeleteAllButton className="mt-5 float-right flex items-center bg-red-600 hover:bg-red-700 text-white text-xs py-2 px-4 rounded" handleClick={handleDeleteCompleted}/>}
         </>
     );
-};
+}
 
-export { AllList, ActiveList, CompletedList };
+export { TodoList };
--- src/containers/main.js
+++ src/containers/main.js
@@ -1,39 +1,38 @@
 import { FormTodo } from '../components/forms/form.js';
-import { ActiveList, CompletedList, AllList } from '../components/lists/list.js';
+import { TodoList } from '../components/lists/list.js';
 
 const Main = props => {
     const todos = props.todos;
     let activeTab = props.activeTab;
     let todoFunctions = props.todoFunctions;
-    const showTodoForm = () => !(activeTab === 'completed')
 
-    const getTodoList = () => {
+    const isCompleted = () => activeTab === 'completed';
+    const getTodos = () => {
         let todoList;
+
         switch (activeTab) {
             case 'active': {
-                todoList = (<ActiveList todos={todos} handleToggleCompleted={todoFunctions.toggleTodoCompleted} />)
+                todoList = todos.filter(t => !t.completed)
                 break;
             }
             case 'completed': {
-                todoList = (<CompletedList todos={todos} handleToggleCompleted={todoFunctions.toggleTodoCompleted} handleDelete={todoFunctions.deleteTodo} handleDeleteCompleted={todoFunctions.deleteCompleted} />)
+                todoList = todos.filter(t => t.completed)
                 break;
             }
             default: {
-                todoList = (<AllList todos={todos} handleToggleCompleted={todoFunctions.toggleTodoCompleted} />)
+                todoList = todos;
             }
         }
-
         return todoList;
     }
 
     return (
         <main className="w-full flex justify-center">
             <div className="w-6/12">
-                {showTodoForm() && <FormTodo addTodo={todoFunctions.addTodo} />}
+                {!isCompleted() && <FormTodo addTodo={todoFunctions.addTodo} />}
                 <div className="w-8/12 m-auto pt-2">
-                    {getTodoList()}
+                    <TodoList todos={getTodos()} allowDelete={isCompleted()} handleToggleCompleted={todoFunctions.toggleTodoCompleted} handleDelete={todoFunctions.deleteTodo} handleDeleteCompleted={todoFunctions.deleteCompleted} />
                 </div>
-
             </div>
         </main>
     );
