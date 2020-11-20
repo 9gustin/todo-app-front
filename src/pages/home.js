c5ac90ebca20ea22dd1197daf8ab4169b9ac6ec6
Agustin Vazquez <agustinvazquez453gmail.com>
2020-11-20 18:57:25 -0300

    add useReducer to manage state in home

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,31 +1,42 @@
 import Header from '../containers/header.js';
 import Main from '../containers/main.js';
 import Footer from '../containers/footer.js';
-import { useState } from 'react';
+import { useReducer, useState } from 'react';
 import { stringComparator } from '../utils/stringComparator.js';
 import {TodosProvider} from '../context/todoContext';
 import Tab from '../models/tab.js';
 
+const initialState = {
+  title: '#TODO',
+  tabs: [
+    new Tab({ name: 'All', selected: true }),
+    new Tab({ name: 'Active', selected: false }),
+    new Tab({ name: 'Completed', selected: false })
+  ],
+  footerText: '9gustin @ DevChallenges.io'
+};
+
+const homeStateReducer = (state, action) => {
+  switch(action.type){
+    case 'TAB_CHANGE':
+      return {
+        ...state,
+        tabs: state.tabs.map(tab => {
+          if (stringComparator(tab.name, action.payload)) tab.selected = true;
+          else tab.selected = false;
+    
+          return tab;
+        })
+      }
+    default: return state;
+  }
+}
+
 const Home = () => {
-  const [globalProps, setGlobalProps] = useState({
-    title: '#TODO',
-    tabs: [
-      new Tab({ name: 'All', selected: true }),
-      new Tab({ name: 'Active', selected: false }),
-      new Tab({ name: 'Completed', selected: false })
-    ],
-    footerText: '9gustin @ DevChallenges.io'
-  });
+  const [globalProps, dispatchGlobalProps] = useReducer(homeStateReducer, initialState);
 
   const handleTabChange = tabName => {
-    let tabs = globalProps.tabs.map(tab => {
-      if (stringComparator(tab.name, tabName)) tab.selected = true;
-      else tab.selected = false;
-
-      return tab;
-    });
-
-    setGlobalProps(props => ({ ...props, tabs }));
+    dispatchGlobalProps({type:'TAB_CHANGE', payload: tabName});
   }
 
   const getActiveTab = () => globalProps.tabs.find(tab => tab.selected);
